64c70c220e4eb1997f85180e2a4c0ef42554aa3f
Honglai <Honglai.Zhan@Vodafone.com.au>
2016-03-01 02:20:44 +1100

    add another two commodities

--- src/test/java/com/thoughtworks/ticket/CommodityTest.java
+++ src/test/java/com/thoughtworks/ticket/CommodityTest.java
@@ -12,4 +12,14 @@ public class CommodityTest {
         assertThat(Commodity.getByCode("ITEM000001"), is(Commodity.COLA));
     }
 
+    @Test
+    public void should_get_APPLE_for_code_ITEM000002() {
+        assertThat(Commodity.getByCode("ITEM000002"), is(Commodity.APPLE));
+    }
+
+    @Test
+    public void should_get_BADMINTON_for_code_ITEM000003() {
+        assertThat(Commodity.getByCode("ITEM000003"), is(Commodity.BADMINTON));
+    }
+
 }
